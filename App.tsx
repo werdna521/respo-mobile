95b55934e454a616b1c67eac164be428fc787173
Andrew Cen <48923485+werdna521@users.noreply.github.com>
2020-05-17 15:49:56 +0700

    Import fonts and license
    1. Added fonts to asset folder
    2. Appended license on top of every codes

--- App.tsx
+++ App.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// Copyright 2020 Andrew Cen
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-export default function App() {
+import React, { useEffect, useState } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import RootNavigator from './navigators/RootNavigator';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+
+const App: React.FC = () => {
+  const [_loading, _setLoading] = useState(true);
+
+  useEffect(() => {
+    const bootstrapAsync = async () => {
+      await Font.loadAsync({
+        'montserrat-bold': require('./assets/fonts/montserrat/Montserrat-Bold.ttf')
+      });
+      _setLoading(false);
+    };
+    bootstrapAsync().then();
+  });
+
+  if (_loading) return <AppLoading/>;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <RootNavigator/>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
